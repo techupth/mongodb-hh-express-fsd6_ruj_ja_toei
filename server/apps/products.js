951d51bf1d1a7afdf0d1504c029ac978af5d5a68
Ja <ja.techup@gmail.com>
2024-06-28 16:12:14 +0700

    feat: Assignment : No SQL Creating APIs

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,63 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
+  const products = await collection.find({}).toArray();
+  return res.json({
+    data: products,
+  });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  const products = await collection.findOne({
+    _id: productId,
+  });
+  return res.json({
+    data: products,
+  });
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+  const newProduct = { ...req.body };
+  await collection.insertOne(newProduct);
 
-productRouter.put("/:id", (req, res) => {});
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  const updatedProduct = { ...req.body };
+  await collection.updateOne(
+    {
+      _id: productId,
+    },
+    { $set: updatedProduct }
+  );
+
+  return res.json({
+    message: "Product has been updated successfully",
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  await collection.deleteOne({
+    _id: productId,
+  });
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
